1b29d126385c7c1e163b7ce243a8126b1a964b3e
nikitasurikov123-code <nikitasurikov123@gmail.com>
2026-03-26 12:39:07 -0600

    cleaned the engine column as it had x "cc" and that is messy

--- KNNmodel.py
+++ KNNmodel.py
@@ -5,11 +5,26 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_absolute_error
 
+#load data
 data_frame = pandas.read_csv("car details v4.csv")
 
+#convert indian rupees to cad
 rupee_to_cad = 0.01525
 data_frame["Price"] = data_frame["Price"] * rupee_to_cad
 
+#remove columns that aren't needed
 data_frame = data_frame.drop(columns = ["Color", "Length", "Width", "Height", "Max Power", "Max Torque"])
 
+#remove "cc" from engine data
 cleaned_engine = []
+for cc in data_frame["Engine"]:
+    if pandas.notna(cc):
+        cc = str(cc)
+        cc = cc.replace("cc", "")
+        cc = cc.strip()
+        cleaned_engine.append(cc)
+    else:
+        cleaned_engine.append(cc)
+
+data_frame["Engine"] = cleaned_engine
+
