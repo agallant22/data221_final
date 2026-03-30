405cf1eaf60d9245c1de5ab7c804fc5d333edd93
nikitasurikov123-code <nikitasurikov123@gmail.com>
2026-03-30 11:20:51 -0600

    Update KNNmodel.py

--- KNNmodel.py
+++ KNNmodel.py
@@ -125,4 +125,4 @@ print(y_pred[similar_car])
 print("Actual price: ")
 print(y_test.iloc[similar_car])
 
-//https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsRegressor.html
+#https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsRegressor.html
